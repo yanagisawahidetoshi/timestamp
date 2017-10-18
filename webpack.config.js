ba31682c2cba997132b0b4e124684e2332a1585c
HidetoshiYanagisawa <yanagisawa@ultrasevenstar.com>
2017-10-18 04:42:52 +0000

    雛形一旦完成

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
-            }
+            },
         ]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'source-map',
 }
