c75a45ae17e04d5084cc5214b03ca8866831a1ec
HidetoshiYanagisawa <yanagisawa@ultrasevenstar.com>
2017-10-18 08:07:57 +0000

    クリップボードコピー実装

--- src/js/timestamp.js
+++ src/js/timestamp.js
@@ -1,11 +1,23 @@
 const Moment = require('moment');
+const Clipboard = require('clipboard');
 
 class Timestamp {
     constructor(base, date = null) {
         this.base = document.getElementsByClassName(base)[0];
+        this.copyTrigger = document.getElementsByClassName('jsc-timestamp-copyTrigger')[0];
+        this.target = this.base.getElementsByClassName('jsc-timestamp-target')[0];
+        
         this.timeStamp = this.convTimestamp(date);
         
         this.setTimeStamp();
+        
+        this.bindEvents();
+    }
+    bindEvents() {
+        this.copyTrigger.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.setClipBoard();
+        });
     }
     convTimestamp(date) {
         if(! date) {
@@ -13,11 +25,15 @@ class Timestamp {
         }
     }
     setTimeStamp() {
-        const target = this.base.getElementsByClassName('jsc-timestamp-target')[0];
-        
-        target.value = this.timeStamp;
+        this.target.value = this.timeStamp;
     }
-    
+    setClipBoard() {
+        if(! this.target.value) {
+            return;
+        }
+        var cp = new Clipboard(".jsc-timestamp-copyTrigger");
+        
+    }
 }
 
 export default Timestamp;
