766b7a36748d7f4aa798473580db3c971853851e
HidetoshiYanagisawa <yanagisawa@ultrasevenstar.com>
2017-10-18 04:15:03 +0000

    ES2015コンパイル設定

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+class hoge {
+    constructor() {
+        alert('A');
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: ['babel-polyfill', './src/js/app.js'],
+    output: {
+        path: __dirname + '/assets/js',
+        filename: "app.js"
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map',
+}
